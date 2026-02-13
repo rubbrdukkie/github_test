23e7206868bcea21586c08c28c438687c1bda525
rubbrdukkie <krishnadathmanu@gmail.com>
2026-02-13 15:39:45 +0100

    Added julia

--- buhbuh.R
+++ buhbuh.R
@@ -13,3 +13,20 @@ for (i in 1:60) {
 # The boundary of the Mandelbrot set is a fractal curve
 # No matter how much you zoom in, the boundary stays "rough" 
 image(x, y, nM, col = hcl.colors(50, "Inferno"), useRaster = TRUE)
+
+# ---- Julia ----
+k <- complex(real = -0.8, imaginary = 0.156)  # Julia parameter
+z <- c
+nJ <- matrix(0, length(x), length(y))
+
+for (i in 1:60) {
+  z <- z^2 + k
+  nJ[nJ == 0 & abs(z) > 2] <- i
+}
+
+# Plot
+par(mfrow = c(1, 2), mar = c(1, 1, 2, 1))
+image(x, y, nM, col = hcl.colors(50, "Inferno"),
+      main = "Mandelbrot", useRaster = TRUE)
+image(x, y, nJ, col = hcl.colors(50, "Inferno"),
+      main = paste("Julia, c =", k), useRaster = TRUE)
